f1b0ac31b3fc8f8e3ba91b4ef2eaac2828c734d0
Nielsm <nielsmedemblik@hotmail.com>
2021-04-02 08:49:27 +0200

    update script path

--- bin/windows/blender.bat
+++ bin/windows/blender.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set __app__="Blender 2.92"
-set __exe__="C:\Program Files\Blender Foundation\Blender 2.92\blender.exe" --python-use-system-env --python "C:\Users\Gebruiker\avalon-setup\git\avalon-core\setup\blender\startup\setup_avalon.py"
+set __exe__="C:\Program Files\Blender Foundation\blender-2.92.0\blender.exe" --python-use-system-env --python "C:\Users\niels\github\setup\git\avalon-core\setup\blender\startup\setup_avalon.py"
 if not exist %__exe__% goto :missing_app
 
 start %__app__% %__exe__% %*
